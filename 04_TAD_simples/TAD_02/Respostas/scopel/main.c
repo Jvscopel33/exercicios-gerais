381047bcaf8feb7c65c0d46c93fb3ea1778011e9
Jvscopel33 <joao.s.ramos@edu.ufes.br>
2026-09-05 23:21:16 -0300

    TAD_02 - finalizado

---
+++ 04_TAD_simples/TAD_02/Respostas/scopel/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "eleitor.h"
+#include "eleicao.h"
+#include "candidato.h"
+
+int main()
+{
+    tEleicao eleicao;
+    eleicao = InicializaEleicao();
+    eleicao = RealizaEleicao(eleicao);
+    ImprimeResultadoEleicao(eleicao);
+    return 0;
+}
